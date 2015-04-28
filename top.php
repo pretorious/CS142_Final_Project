c27d005933960f852799d46d4878a7518d909c4d
jdavis30 <jasperdavis95@gmail.com>
2015-04-28 15:20:40 -0400

    added top.php

---
+++ top.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<title>Snag - Get Connected with UVM Students and Events</title>
+		<meta charset="utf-8">
+		<meta name="authors" content="Preston Libby, Jasper Davis">
+		<meta name="description" content="Snag Social Media Site, Helping Connect UVM Students
+											to events and interests all over campus.">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+
+		<link rel="stylesheet" href="stylesheet.css" type="text/css" media="screen" title="Main">
+	</head>
+	
