91d48ad435148fd321520c4bb521817c9efb5ffc
Preston Libby <preston.libby@uvm.edu>
2015-05-05 14:47:18 -0400

    Final Changes
    Phew, we're done!

---
+++ testing_results.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="author" content="Preston Libby, Jasper Davis">
+        <meta name="description" content="Snag Usability Testing">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <link rel="stylesheet" href="results.css" type="text/css" media="screen" title="Main">
+    </head>
+    <body>
+
+        <h1>Testing Results for Final Project</h1>
+
+        <h2>Overview</h2>
+        <p>
+            Through 4 separate testers over 2 rounds of testing with 2 testers per round, we had very little by way of suggestions for change.
+            Primarily user feedback was that the process was quite simply and straightforward, with little to no confusion. 
+            Some small suggestions included:
+        </p>
+        <ul>
+            <li>adding a notification as to whether the password required certain qualities</li>
+            <li>improving color/asthetics</li>
+        </ul>
+
+
+        <h2>Videos</h2>
+
+        <h3>Round 1</h3>
+        <ul class="videos">
+            <li><a href="https://youtu.be/zYcwvJU2nHc" target="blank">Test 1</a></li>
+            <li><a href="https://www.youtube.com/watch?v=KDe6Y-FfHZE" target="blank">Test 2</a></li>
+        </ul>
+
+        <h3>Round 2</h3>
+        <ul class="videos">
+            <li><a href="https://www.youtube.com/watch?v=iEQFHsIgnL0" target="blank">Test1</a></li>
+            <li><a href="https://youtu.be/7KNaCgZ0J34" target="blank">Test 2</a></li>
+        </ul>
+    </body>
+</html>
